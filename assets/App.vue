84194038c6f2762fcc482ca7bdc6b83d9d912022
MCU-LAB <30004389+MCU-LAB@users.noreply.github.com>
2026-04-15 16:39:08 +0800

    Update App.vue

--- assets/App.vue
+++ assets/App.vue
@@ -18,7 +18,7 @@
     <div class="app-bar">
       <a class="app-title-container" style="display: flex; align-items: center;" href="/">
         <img src="/assets/homescreen.png" alt="AinkDrive" style="height: 24px" />
-        <h1 class="app-title" style="font-size: 20px;margin: 0 25px 0 8px; user-select: none;">FlareDrive</h1>
+        <h1 class="app-title" style="font-size: 20px;margin: 0 25px 0 8px; user-select: none;">AinkDrive</h1>
       </a>
 
       <input type="search" v-model="search" aria-label="Search" placeholder="🍿 输入以全局搜索文件" />
